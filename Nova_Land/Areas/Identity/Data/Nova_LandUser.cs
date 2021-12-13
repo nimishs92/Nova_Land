b9008b861a0955461e2e0d6a53d40a0899e8ada7
nimishs92 <nimishs92@gmail.com>
2021-12-12 20:25:30 -0400

    Add project files.

---
+++ Nova_Land/Areas/Identity/Data/Nova_LandUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Nova_Land.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the Nova_LandUser class
+    public class Nova_LandUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "Text")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "Text")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Nova_Land/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nova_Land.Areas.Identity.Data;
+using Nova_Land.Data;
+
+[assembly: HostingStartup(typeof(Nova_Land.Areas.Identity.IdentityHostingStartup))]
+namespace Nova_Land.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseMySQL(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<Nova_LandUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
