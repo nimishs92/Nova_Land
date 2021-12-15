192bef947a55d66997ef8fdf1ebd0aee44c8836c
nimishs92 <nimishs92@gmail.com>
2021-12-15 19:26:44 -0400

    Added new custom validator class

---
+++ Nova_Land/Validators/CustomCreditCardAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Nova_Land.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nova_Land.Validators
+{
+    public class CustomCreditCardAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+        ValidationContext validationContext)
+        {
+            var payment = (Payment)validationContext.ObjectInstance;
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
